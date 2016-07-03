17c27f156b80f99682004c08b279d9fc0d43f162
jdgonzales09 <jd_gonzales@icloud.com>
2016-07-03 00:54:03 -0500

    Add get_me method to retrive user data after authorization. Complete.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,6 @@
 require "kele/version"
 require "HTTParty"
-
+require "json"
 
 class Kele
     include HTTParty
@@ -11,5 +11,9 @@ class Kele
         @auth_token = response["auth_token"]
     end
     
+    def get_me
+        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        @user_info = JSON.parse(response.body)
+    end
     
 end
