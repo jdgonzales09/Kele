89be11c3236b9280b031b09591db987881ae15bc
jdgonzales09 <jd_gonzales@icloud.com>
2016-07-05 12:43:33 -0500

    Add get_checkpoints and get_roadmaps, JSON blob is crazy with roadmaps. Checking on problem

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,9 +16,19 @@ class Kele
         @user_info = JSON.parse(response.body)
     end
 
-    def get_mentor_availability(id)
-        response = self.class.get('/mentors/' + id.to_s + '/student_availability', headers: { "authorization" => @auth_token})
+    def get_mentor_availability(mentor_id)
+        response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: { "authorization" => @auth_token})
         @mentor_availability = JSON.parse(response.body)
+    end
+
+    def get_roadmap(roadmap_id)
+        response = self.class.get('/roadmaps/' + roadmap_id.to_s, headers: { "authorization" => @auth_token})
+        @roadmap = JSON.parse(response.body)
+    end
+
+    def get_checkpoint(checkpoint_id)
+        response = self.class.get('/checkpoints/' + checkpoint_id.to_s, headers: { "authorization" => @auth_token})
+        @checkpoint = JSON.parse(response.body)
     end
     
 end
