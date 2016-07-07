97e9a5b63c52f981b74721ba35d33e65c60ff16f
jdgonzales09 <jd_gonzales@icloud.com>
2016-07-06 22:37:37 -0500

    Add ability to get all or a page of messages.

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,19 +11,34 @@ class Kele
     base_uri 'https://www.bloc.io/api/v1'
 
     def initialize(email, password)
-        response = self.class.post('/sessions', body: { "email": email, "password": password } )
+        response = self.class.post("/sessions", body: { "email": email, "password": password } )
         @auth_token = response["auth_token"]
     end
-    
+
     def get_me
-        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
         @user_info = JSON.parse(response.body)
     end
 
     def get_mentor_availability(mentor_id)
-        response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: { "authorization" => @auth_token})
+        response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token})
         @mentor_availability = JSON.parse(response.body)
     end
 
-    
+    def get_messages(page = nil)
+      if page.nil?
+        response = self.class.get("/message_threads", headers: { "authorization" => @auth_token })
+        pageCount = JSON.parse(response.body)
+        totalPages = (1..(response["count"]/10 + 1)).map do |n|
+          self.class.get("/message_threads?page=#{n}", headers: { "authorization" => @auth_token })
+                    end
+          totalPages
+      else
+          response = self.class.get("/message_threads?page=#{page}", headers: { "authorization" => @auth_token })
+          @messages = JSON.parse(response.body)
+      end
+    end
+
+
+
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -5,12 +5,12 @@ module Roadmap
     base_uri 'https://www.bloc.io/api/v1'
     
     def get_roadmap(roadmap_id)
-        response = self.class.get('/roadmaps/' + roadmap_id.to_s, headers: { "authorization" => @auth_token})
+        response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token})
         @roadmap = JSON.parse(response.body)
     end
 
     def get_checkpoint(checkpoint_id)
-        response = self.class.get('/checkpoints/' + checkpoint_id.to_s, headers: { "authorization" => @auth_token})
+        response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token})
         @checkpoint = JSON.parse(response.body)
     end
 
